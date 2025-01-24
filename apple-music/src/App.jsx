9c4292133a932d86613085530c871eb55992d2a2
Sachidu-2001 <fsachi0001@gmail.com>
2025-01-24 14:36:51 +0100

    sidebar

--- apple-music/src/App.jsx
+++ apple-music/src/App.jsx
@@ -1,20 +1,22 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import AppleBar from './components/AppleBar.jsx'
-import AppleNovita from './components/AppleNovita.jsx';
-import AppleOthers from './components/AppleOthers.jsx';
-import AppleFooter from './components/AppleFooter.jsx';
-
+import "bootstrap/dist/css/bootstrap.min.css";
+import AppleBar from "./components/AppleBar.jsx";
+import AppleNovita from "./components/AppleNovita.jsx";
+import AppleOthers from "./components/AppleOthers.jsx";
+import AppleFooter from "./components/AppleFooter.jsx";
 
 function App() {
-
   return (
     <>
-    <AppleBar/>
-    <AppleNovita/>
-    <AppleOthers/>
-    <AppleFooter/>
+      <div className="d-lg-flex">
+        <AppleBar />
+        <div>
+          <AppleNovita />
+          <AppleOthers />
+          <AppleFooter />
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- apple-music/src/components/AppleBar.jsx
+++ apple-music/src/components/AppleBar.jsx
@@ -1,23 +1,31 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import MusicLogo from '../assets/logos/music.svg'
+import MusicLogo from "../assets/logos/music.svg";
 
 function AppleBar() {
   return (
-    <Navbar expand="lg" className="bg-warning py-3">
-      <Container>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Novità</Nav.Link>
-            <Nav.Link href="#link">Radio</Nav.Link>
-          </Nav>
-        </Navbar.Collapse>
-        <Navbar.Brand href="#home" className="m-auto text-white"><img src={MusicLogo}/></Navbar.Brand>
-        <p className="h3 my-auto">Accedi</p>
-        
+    <Navbar
+      expand="lg"
+      className="bg-warning py-3 d-flex align-items-lg-start "
+    >
+      <Container className="d-flex flex-lg-column-reverse">
+        <div>
+          <Navbar.Toggle aria-controls="basic-navbar-nav" />
+          <Navbar.Collapse id="basic-navbar-nav">
+            <Nav className="me-auto d-flex flex-column ">
+              <input placeholder="Cerca"/>
+              <Nav.Link href="#home">Home</Nav.Link>
+              <Nav.Link href="#link">Novità</Nav.Link>
+              <Nav.Link href="#link">Radio</Nav.Link>
+            </Nav>
+          </Navbar.Collapse>
+        </div>
+
+        <Navbar.Brand href="#home" className="m-auto ms-lg-2">
+          <img src={MusicLogo} /> <br />
+        </Navbar.Brand>
+        <p className="h3 my-auto d-lg-none">Accedi</p>
       </Container>
     </Navbar>
   );
