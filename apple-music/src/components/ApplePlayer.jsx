5e935ddd6735886d91d386ba82500c7c44502aa9
Sachidu-2001 <fsachi0001@gmail.com>
2025-01-24 16:47:22 +0100

    player

--- apple-music/src/components/ApplePlayer.jsx
+++ apple-music/src/components/ApplePlayer.jsx
@@ -1,15 +1,21 @@
 import { Container } from "react-bootstrap";
-import First from '../assets/images/1a.png'
+import First from "../assets/images/2a.png";
+import * as Icon from "react-bootstrap-icons";
 
 function ApplePlayer() {
-    return(
-        <Container fluid>
-            <div>
-                <img src={First}/>
-            </div>
-        </Container>
-
-    )
+  return (
+    <Container fluid className="bg-dark">
+      <div className="d-flex justify-content-between py-2">
+        <div>
+          <img src={First} style={{ height: "5.5em" }} className="rounded-4"/>
+        </div>
+        <div className="d-flex align-items-center">
+          <Icon.PlayFill className="display-1 text-secondary me-4" />
+          <Icon.FastForwardFill className="display-3 text-secondary" />
+        </div>
+      </div>
+    </Container>
+  );
 }
 
-export default ApplePlayer
+export default ApplePlayer;
